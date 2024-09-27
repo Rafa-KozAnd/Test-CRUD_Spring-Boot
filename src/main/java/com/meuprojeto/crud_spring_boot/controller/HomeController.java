7ff8637db10045a5cc4092b2460068166f508de7
Rafael KozAnd <rafandreola@hotmail.com>
2024-09-27 19:58:12 -0300

    Updated

--- src/main/java/com/meuprojeto/crud_spring_boot/controller/HomeController.java
+++ src/main/java/com/meuprojeto/crud_spring_boot/controller/HomeController.java
@@ -1,5 +1,9 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
 public class HomeController {
@@ -7,5 +11,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handleNotFound() {
+        return new ResponseEntity<>("Página não encontrada", HttpStatus.NOT_FOUND);
     }
 }
